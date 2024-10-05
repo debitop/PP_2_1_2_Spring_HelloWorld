899451aa4fc684986cb48c7a6266e162c498a6d8
Aleksandr <89263693878@mail.ru>
2024-10-05 08:39:23 +0300

    Init commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,15 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld helloBean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld helloBean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(helloBean1.getMessage());
+        System.out.println(catBean1.getMessage());
+
+        System.out.println("бины helloworld равны? " + (helloBean1 == helloBean2));
+        System.out.println("бины cat равны? " + (catBean1 == catBean2));
     }
 }
